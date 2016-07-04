b6398d94f5dd5d0053c31048ec576c1ddb2b380b
Douglas Bastos <douglashsb@gmail.com>
2016-07-04 00:09:11 -0300

    Ajustando models game

--- core/models/game.py
+++ core/models/game.py
@@ -6,13 +6,13 @@ from .rounds import Rounds
 
 class Game(models.Model):
     name = models.CharField('Nome', max_length=255)
-    team_home = models.ForeignKey(Team)
-    team_visitor = models.ForeignKey(Team)
-    result_home = models.PositiveSmallIntegerField()
-    result_visitor = models.PositiveSmallIntegerField()
+    team_home = models.ForeignKey(Team, related_name='Mandante')
+    team_visitor = models.ForeignKey(Team, related_name='Visitante')
+    result_home = models.PositiveSmallIntegerField('Resultado Mandante', blank=True, null=True)
+    result_visitor = models.PositiveSmallIntegerField('Resultado Visitante', blank=True, null=True)
     rounds = models.ForeignKey(Rounds)
-    local = models.CharField('Local', max_length=255)
-    date = models.DateTimeField('Data e Hora')
+    local = models.CharField('Local', max_length=255, blank=True, null=True)
+    date = models.DateTimeField('Data e Hora', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Competição'
