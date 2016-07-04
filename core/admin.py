ab5c47c50f799c697a9d5789974bcc0f010ec6b1
Douglas Bastos <douglashsb@gmail.com>
2016-07-04 00:38:51 -0300

    Adiciona admin para models basic

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,45 @@
-from django.contrib import admin
+# coding: utf-8
 
-# Register your models here.
+from django.contrib import admin
+from .models import Game, Competition, Rounds, Team, CompetitionHasTeam
+
+
+class GameAdmin(admin.ModelAdmin):
+    fieldsets = ((None, {
+        'fields': ('name',),
+    }),)
+
+
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fieldsets = ((None, {
+        'fields': ('name',),
+    }),)
+
+
+class RoundsAdmin(admin.ModelAdmin):
+    list_display = ('number', 'competition')
+    fieldsets = ((None, {
+        'fields': ('number', 'competition'),
+    }),)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fieldsets = ((None, {
+        'fields': ('name',),
+    }),)
+
+
+class CompetitionHasTeamAdmin(admin.ModelAdmin):
+    list_display = ('competition', 'team')
+    fieldsets = ((None, {
+        'fields': ('competition', 'team'),
+    }),)
+
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Competition, CompetitionAdmin)
+admin.site.register(Rounds, RoundsAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(CompetitionHasTeam, CompetitionHasTeamAdmin)
